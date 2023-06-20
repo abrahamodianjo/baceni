5d92138b5a80cd1d86dc93f0657a72a08c53226a
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-21 00:03:30 +0100

    multi auth breeze -2

---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+    <h1> Admin Dashboard</h1>
+</body>
---
+++ resources/views/vendor/vendor_dashboard.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>Vendor Dashboard</title>
+</head>
+<body>
+    <h1> Vendor Dashboard</h1>
+</body>
