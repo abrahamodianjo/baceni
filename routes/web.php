0d147745ca8d2ec3c2768ac4059a7af47ad79e88
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-21 01:35:58 +0100

    set up dashboard page

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,13 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+// admin dashboard
+
 Route::middleware(['auth', 'role:admin'])->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');   
 });
 
-
+//vendor dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function(){
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');    
 });
