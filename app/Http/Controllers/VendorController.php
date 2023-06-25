60987c19e9dd24f0a812b8655b31ea6382b5c0e5
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-25 11:36:25 +0100

    vendor logout functionality done

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class VendorController extends Controller
@@ -11,4 +12,22 @@ class VendorController extends Controller
         return view('vendor.index');
 
     }//End AdminDashboard Method
+
+    public function VendorLogin(){
+
+        return view('vendor.vendor_login');
+    }//End VendorLogin Method
+
+    
+    public function VendorDestroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/vendor/login');
+    }//End logout Method
+
 }
--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
 //vendor dashboard
 Route::middleware(['auth', 'role:vendor'])->group(function(){
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');    
+    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
+   
 });
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
