f1439df63ac1b880022e3c7bfda1aee948cd8944
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-28 04:16:57 +0100

    frontend user design

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function UserDashboard(){
+        $id = Auth::user()->id;
+        $userData = User::find($id);
+        return view('index',compact('userData'));
+
+    } //End Method
+
+    
+    public function UserProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+
+        $data->name = $request->name;
+        $data->username = $request->username;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/user_images'),$filename);
+            $data['photo'] = $filename;
+
+        }
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin profile updated successfully', 
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+
+    }//End Method
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\VendorController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,17 @@ Route::get('/', function () {
     return view('frontend.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/dashboard', [UserController::class, 'UserDashboard'])->name('admin.dashboard');
+
+    Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
+    
+}); //Group milldware End
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
