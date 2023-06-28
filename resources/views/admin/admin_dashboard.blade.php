0e018d4302bda335472abc28de8ebaee8acfbbbb
abrahamodianjo <odianjoabraham@outlook.com>
2023-06-28 20:44:40 +0100

    added the validation message for add brand section

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -77,6 +77,7 @@
 	  <script src="{{asset('adminbackend/assets/js/index.js')}}"></script>
 	<!--app JS-->
 	<script src="{{asset('adminbackend/assets/js/app.js')}}"></script>
+	<script src="{{asset('adminbackend/assets/js/validate.min.js')}}"></script>
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
 <script>
