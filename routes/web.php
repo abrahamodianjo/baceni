6581ba9a93cbc5edc819fddd54fade1ea5fadac5
abrahamodianjo <odianjoabraham@outlook.com>
2023-08-07 07:32:53 +0100

    worked on the product upload controller

--- routes/web.php
+++ routes/web.php
@@ -145,6 +145,7 @@ Route::controller(ProductController::class)->group(function(){
     
     Route::get('/all/product', 'AllProduct')->name('all.product');
     Route::get('/add/product', 'AddProduct')->name('add.product');
+    Route::post('/store/product', 'StoreProduct')->name('store.product');
     
 
 
