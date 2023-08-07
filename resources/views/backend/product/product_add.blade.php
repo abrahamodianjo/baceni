30a175b5d140aa0fd62ed215dd7a075af0bb7db9
abrahamodianjo <odianjoabraham@outlook.com>
2023-08-07 02:59:12 +0100

    load the category, brand, vendor and sub category -2

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -124,9 +124,9 @@
 					<label for="inputCollection" class="form-label">Product Subcategory</label>
 					<select name="subcategory_id" class="form-select" id="inputCollection">
 						<option></option>
-						<option value="1">One</option>
-						<option value="2">Two</option>
-						<option value="3">Three</option>
+						@foreach($categories as $cat)
+						<option value="{{$cat->id}}">{{ $cat->category_name }}</option>
+						@endforeach
 					  </select>
 				  </div>
                   
@@ -249,5 +249,30 @@ function mainThamUrl(input) {
      
     </script>
 
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('select'[name="category_id"]').on('change', function() {
+		
+			var category_id = $(this).val();
+			if (category_id){
+				$.ajax({
+					url: "{{ url('/subcategory/ajax') }}/"+category_id,
+					type: "GET",
+					dataType: "json",
+					success:function(data) {
+						$('select[name="subcategory_id"]').html('');
+						var d =$('select[name="subcategory_id"]').empty();
+						$.each(data, function(key, value){
+                            $('select[name="subcategory_id"]').append('<option value="'+ value.id +'">'+ value.subcategory_name +'</option>');
+                        });
+					},
+				});
+
+				} else {
+					alert('danger');
+				}
+			});
+		});
+</script>
 
 @endsection
