f68d34682d76bc3bf448cfee08a4d51064d37187
abrahamodianjo <odianjoabraham@outlook.com>
2023-12-29 18:33:10 +0000

    update the ajax for quick view

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -9,7 +9,8 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="" />
     
-    <meta name="csrf-token" content="{{ csrf-token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -89,6 +90,68 @@
             }
         })
         /// Start product view with Modal 
+
+        function productView(id){
+        // alert(id)
+        $.ajax({
+            type: 'GET',
+            url: '/product/view/modal/'+id,
+            dataType: 'json',
+            success:function(data){
+                               // console.log(data)
+            $('#pname').text(data.product.product_name);
+            $('#pprice').text(data.product.selling_price);
+            $('#pcode').text(data.product.product_code);
+            $('#pcategory').text(data.product.category.category_name);
+            $('#pbrand').text(data.product.brand.brand_name);
+            $('#pimage').attr('src','/'+data.product.product_thambnail );
+            
+            // Product Price 
+            if (data.product.discount_price == null) {
+                $('#pprice').text('');
+                $('#oldprice').text('');
+                $('#pprice').text(data.product.selling_price);
+            }else{
+                $('#pprice').text(data.product.discount_price);
+                $('#oldprice').text(data.product.selling_price); 
+            } // end else
+
+             /// Start Stock Option
+             if (data.product.product_qty > 0) {
+                $('#aviable').text('');
+                $('#stockout').text('');
+                $('#aviable').text(' Available for purchase');
+            }else{
+                $('#aviable').text('');
+                $('#stockout').text('');
+                $('#stockout').text(' Out of stock');
+            } 
+            ///End Start Stock Option
+             ///Size 
+             $('select[name="size"]').empty();
+             $.each(data.size,function(key,value){
+                $('select[name="size"]').append('<option value="'+value+' ">'+value+'  </option')
+                if (data.size == "") {
+                    $('#sizeArea').hide();
+                }else{
+                     $('#sizeArea').show();
+                }
+             }) // end size
+                     ///Color 
+               $('select[name="color"]').empty();
+             $.each(data.color,function(key,value){
+                $('select[name="color"]').append('<option value="'+value+' ">'+value+'  </option')
+                if (data.color == "") {
+                    $('#colorArea').hide();
+                }else{
+                     $('#colorArea').show();
+                }
+             }) // end size
+
+
+            }
+        })
+    }
      </script>
     
     
