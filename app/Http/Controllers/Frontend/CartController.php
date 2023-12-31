e2b65fe28f2a51ea183aff9f62ba0979117ea45f
abrahamodianjo <odianjoabraham@outlook.com>
2023-12-31 11:22:15 +0000

    developed the cart page with ajax

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -125,5 +125,43 @@ class CartController extends Controller
 
     }// End Method
 
+    public function GetCartProduct(){
+
+        $carts = Cart::content();
+        $cartQty = Cart::count();
+        $cartTotal = Cart::total();
+
+        return response()->json(array(
+            'carts' => $carts,
+            'cartQty' => $cartQty,  
+            'cartTotal' => $cartTotal
+
+        ));
+
+    }// End Method
+
+    public function CartRemove($rowId){
+        Cart::remove($rowId);
+        return response()->json(['success' => 'Successfully Remove From Cart']);
+
+    }// End Method
+
+    public function CartDecrement($rowId){
+
+        $row = Cart::get($rowId);
+        Cart::update($rowId, $row->qty -1);
+
+        return response()->json('Decrement');
+
+    }// End Method
+
+    public function CartIncrement($rowId){
+
+        $row = Cart::get($rowId);
+        Cart::update($rowId, $row->qty +1);
+
+        return response()->json('Increment');
+
+    }// End Method
 
 }
