380ad8391ed2e1b86bf3a268d339d0d14d6664b4
abrahamodianjo <odianjoabraham@outlook.com>
2024-02-19 16:44:47 +0000

    made the invoice downloadable on PDF

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -9,6 +9,7 @@ use App\Models\User;
 use App\Models\Order;
 use App\Models\OrderItem;
 use Illuminate\Support\Facades\Hash;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class AllUserController extends Controller
 {
@@ -39,7 +40,18 @@ class AllUserController extends Controller
 
 }// End Method 
 
+public function UserOrderInvoice($order_id){
 
+    $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
+    $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
+
+    $pdf = Pdf::loadView('frontend.order.order_invoice', compact('order','orderItem'))->setPaper('a4')->setOption([
+            'tempDir' => public_path(),
+            'chroot' => public_path(),
+    ]);
+    return $pdf->download('invoice.pdf');
+
+}// End Method 
 
 
 }
