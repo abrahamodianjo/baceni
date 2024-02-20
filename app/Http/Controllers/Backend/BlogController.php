34b97fa94dc36ee7e94fe6ebcc37fd79a7eabe09
abrahamodianjo <odianjoabraham@outlook.com>
2024-02-20 20:08:35 +0000

    set up the blog and blog cateory for frontend

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -203,4 +203,37 @@ class BlogController extends Controller
 
 
 
+
+
+
+
+    //////////////////// Frontend Blog All Method //////////////
+
+
+    public function AllBlog(){
+        $blogcategoryies = BlogCategory::latest()->get();
+        $blogpost = BlogPost::latest()->get();
+        return view('frontend.blog.home_blog',compact('blogcategoryies','blogpost'));
+    }// End Method 
+
+    public function BlogDetails($id,$slug){
+        $blogcategoryies = BlogCategory::latest()->get();
+        $blogdetails = BlogPost::findOrFail($id);
+        $breadcat = BlogCategory::where('id',$id)->get();
+        return view('frontend.blog.blog_details',compact('blogcategoryies','blogdetails','breadcat'));
+
+    }// End Method 
+
+    public function BlogPostCategory($id,$slug){
+
+        $blogcategoryies = BlogCategory::latest()->get();
+        $blogpost = BlogPost::where('category_id',$id)->get();
+        $breadcat = BlogCategory::where('id',$id)->get();
+        return view('frontend.blog.category_post',compact('blogcategoryies','blogpost','breadcat'));
+
+    }// End Method 
+
+
+
+
 }
