657c873f71115039ad4f3f42735ee8f0d2bdd1e5
abrahamodianjo <odianjoabraham@outlook.com>
2024-04-18 19:05:01 +0100

    Updated the footer of the frontend

--- resources/views/frontend/body/footer.blade.php
+++ resources/views/frontend/body/footer.blade.php
@@ -114,19 +114,19 @@
                         <div class="logo mb-30">
                             <a href="index.html" class="mb-15"><img src="{{ asset($setting->logo) }}"
                                     alt="logo" /></a>
-                            <p class="font-lg text-heading">Awesome grocery store website Ecommerce</p>
+                            <p class="font-lg text-heading">Our goal is to provide you with all you need in Nigeria</p>
                         </div>
                         <ul class="contact-infor">
                             <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-location.svg') }}"
                                     alt="" /><strong>Address: </strong> <span> {{ $setting->company_address }}
                                 </span></li>
                             <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-contact.svg') }}"
-                                    alt="" /><strong>Call Us:</strong><span>{{ $setting->phone_one }}</span>
+                                    alt="" /><strong>Call Us: </strong><span>{{ $setting->phone_one }}</span>
                             </li>
                             <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-email-2.svg') }}"
-                                    alt="" /><strong>Email:</strong><span>{{ $setting->email }}</span></li>
+                                    alt="" /><strong>Email: </strong><span>{{ $setting->email }}</span></li>
                             <li><img src="{{ asset('frontend/assets/imgs/theme/icons/icon-clock.svg') }}"
-                                    alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
+                                    alt="" /><strong>Hours: </strong><span>10:00 - 18:00, Mon - Sat</span></li>
                         </ul>
                     </div>
                 </div>
@@ -186,9 +186,9 @@
             <div class="col-12 mb-30">
                 <div class="footer-bottom"></div>
             </div>
-            <div class="col-xl-4 col-lg-6 col-md-6">
-                <p class="font-sm mb-0">&copy; 2022, <strong class="text-brand">Ecommerce</strong> -
-                    {{ $setting->copyright }}</p>
+            <div class="col-xl-4 col-lg-7 col-md-6">
+                <p class="font-sm mb-0">{{ $setting->copyright }}<strong class="text-brand"> &copy; 2024</strong> 
+                    </p>
             </div>
             <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
 
@@ -199,24 +199,18 @@
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                 <div class="mobile-social-icon">
-                    <h6>Follow Us</h6>
-                    <a href="{{ $setting->facebook }}"><img
+                    <h6>Follow Us </h6>
+                    <a href="http://www.facebook.com/{{ $setting->facebook }}"><img
                             src="{{ asset('frontend/assets/imgs/theme/icons/icon-facebook-white.svg') }}"
                             alt="" /></a>
-                    <a href="{{ $setting->twitter }}"><img
+                    <a href="http://www.instagram.com/{{ $setting->twitter }}"><img
+                            src="{{ asset('frontend/assets/imgs/theme/icons/icon-instagram-white.svg') }}"
+                            alt="" /></a>                   
+                    <a href="http://www.twitter.com/{{ $setting->youtube }}"><img
                             src="{{ asset('frontend/assets/imgs/theme/icons/icon-twitter-white.svg') }}"
                             alt="" /></a>
-                    <a href="#"><img
-                            src="{{ asset('frontend/assets/imgs/theme/icons/icon-instagram-white.svg') }}"
-                            alt="" /></a>
-                    <a href="#"><img
-                            src="{{ asset('frontend/assets/imgs/theme/icons/icon-pinterest-white.svg') }}"
-                            alt="" /></a>
-                    <a href="{{ $setting->youtube }}"><img
-                            src="{{ asset('frontend/assets/imgs/theme/icons/icon-youtube-white.svg') }}"
-                            alt="" /></a>
                 </div>
-                <p class="font-sm">Up to 15% discount on your first subscribe</p>
+                {{-- <p class="font-sm">Up to 15% discount on your first subscribe</p> --}}
             </div>
         </div>
     </div>
